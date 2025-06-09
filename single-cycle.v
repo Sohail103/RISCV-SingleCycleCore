fe47fce2ad63bb20350dca58cf04d0af5f3db08f
Sohail <sohailraj.satapathy@gmail.com>
2025-06-09 10:07:37 +0530

    Module to increment program counter

--- single-cycle.v
+++ single-cycle.v
@@ -12,5 +12,15 @@ module Program_Counter(clk, reset, PC_in, PC_out);
       else
         PC_out <= PC_in;
     end
+    
+endmodule
+
+// PC + 4
+module PCplus4(fromPC, NexttoPC);
+
+  input [31:0] fromPC;
+  output [31:0] NexttoPC;
+
+  assign NexttoPC = 4 + fromPC;
 
 endmodule
