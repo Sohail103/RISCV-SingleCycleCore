d9ce3f5cba6f589679169b6404127f7a49e28c25
Sohail <sohailraj.satapathy@gmail.com>
2025-06-13 08:16:53 +0530

    Added multiplexer definition

--- single-cycle.v
+++ single-cycle.v
@@ -183,3 +183,14 @@ module Data_Memory(clk, reset, MemWrite, MemRead, read_address, Write_data, MemD
   assign MemData_out = (MemRead)?D_Memory[read_address]:32'b00;
 
 endmodule
+
+// Multiplexers
+module Mux1(sel1, A1, B1, Mux1_out);
+
+  input sel1;
+  input [31:0] A1, B1;
+  output [31:0] Mux1_out;
+
+  assign Mux1_out = (sel1==1'b0)?A1:B1;
+
+endmodule
