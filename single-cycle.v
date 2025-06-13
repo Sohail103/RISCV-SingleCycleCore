a3c2c8bf46528b19399f4cabafc2ed347e2d7fb2
Sohail <sohailraj.satapathy@gmail.com>
2025-06-13 08:24:32 +0530

    Added adder

--- single-cycle.v
+++ single-cycle.v
@@ -224,3 +224,13 @@ module AND_logic(branch, zero, and_out);
   assign and_out = branch & zero;
 
 endmodule
+
+// Adder
+module Adder(in_1, in_2, Sum_out);
+
+  input [31:0] in_1, in_2;
+  output [31:0] Sum_out;
+
+  assign Sum_out = in_1 + in_2;
+
+endmodule
